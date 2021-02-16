0cd485dd100f380bc862379819617da95814ab03
Taylor Treece <taylor.treece@protonmail.com>
2021-02-16 11:04:53 -0600

    commit tree d71208a7797f2c582d1ce7a3eacbc8ae7564bd02

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+insert into bears (id,name,age,gender,color,temperament,alive) values (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
+insert into bears (id,name,age,gender,color,temperament,alive) values (2,"Rowdy", 10, "M", "black", "intense", 1);
+insert into bears (id,name,age,gender,color,temperament,alive) values (3,"Tabitha", 6, "F", "dark brown", "Nice", 1);
+insert into bears (id,name,age,gender,color,temperament,alive) values (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0);
+insert into bears (id,name,age,gender,color,temperament,alive) values (5,"Melissa", 13, "F", "dark brown", "goofy", 1);
+insert into bears (id,name,age,gender,color,temperament,alive) values (6,"Grinch", 2, "M", "Black", "Grinchy", 1);
+insert into bears (id,name,age,gender,color,temperament,alive) values (7,"Wendy", 6, "F", "Blue", "naive", 1);
+insert into bears (id,name,age,gender,color,temperament,alive) values (8,null, 20, "M", "black", "aggressive", 0);
